1a41da2d0d96da202df89f916327eab1580078a1
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2023-03-24 17:40:51 -0300

    feat: add ThreadPoolExecutor call in PostToTxtThreadExecutor component

--- src/post_to_txt_thread_executor.py
+++ src/post_to_txt_thread_executor.py
@@ -1,4 +1,4 @@
-''' This module defines the JsonToTxtThreadExecutor class '''
+''' This module defines the PostToTxtThreadExecutor class '''
 
 from concurrent import futures
 from concurrent.futures import ThreadPoolExecutor
@@ -6,26 +6,28 @@ from concurrent.futures import ThreadPoolExecutor
 from typing import Generator, List
 
 from file_handler import FileHandler
+from mappers import map_post_dto_to_post_dict
+from post import Post
 
-class JsonToTxtThreadExecutor:
-    ''' JsonToTxtThreadExecutor class '''
+class PostToTxtThreadExecutor:
+    ''' PostToTxtThreadExecutor class '''
 
     file_handler: FileHandler = None
-    jsons: List[dict] = []
+    posts: List[Post] = []
 
     MAX_THREADS: int = 4
 
-    def __init__(self, file_handler: List[dict], jsons: List[dict]) -> None:
+    def __init__(self, file_handler: FileHandler, posts: List[Post]) -> Generator[str, str, str]:
         self.file_handler = file_handler
-        self.jsons = jsons
+        self.posts = posts
 
-    def execute_concurrently(self) -> None:
+    def execute_concurrently(self) -> Generator:
         ''' Execute the following task concurrently '''
 
         with ThreadPoolExecutor(max_workers=self.MAX_THREADS) as executor:
             save_file_json_to_txt_futures = [
-                executor.submit(self.file_handler.save_json_as_txt, json)
-                for json in self.jsons
+                executor.submit(self.file_handler.save_json_as_txt, map_post_dto_to_post_dict(post))
+                for post in self.posts
             ]
 
             for future in futures.as_completed(save_file_json_to_txt_futures):
